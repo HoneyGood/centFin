3da4e3b358c5960deea7776646107adb677a4c2b
BarryWin <slavon.v21@gmail.com>
2019-06-21 23:34:29 +0300

    slideDown button

--- js/common.js
+++ js/common.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     //Code here
 
+//slideDown
+    $(".m3-scroll-arr").click(function (e) {
+        var destination = $("#post-11").offset().top;
+        $('body, html').animate({scrollTop: destination}, 800);
+        return false;
+    })
 });
